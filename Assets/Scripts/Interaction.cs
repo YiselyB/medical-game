63bf952303d0b8d7d48be1b621174d351d396cf6
Yisely <yisely@boominmedia.biz>
2024-07-22 18:57:21 -0700

    Added Health Bar
    + Added Health Bar
    ~ Modified Interaction Script to modify health

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -1,11 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Interaction : MonoBehaviour, IInteractable
 {
+    public Image healthBar;
+    public float healthAmount = 10f;
+
+    public void Start()
+    {
+        healthBar.fillAmount = healthAmount / 100f;
+    }
+
     public void Interact()
     {
-        Debug.Log(Random.Range(0, 100));
+        //Debug.Log(Random.Range(0, 100));
+        AddHealth(10);
+    }
+
+    public void AddHealth(float healAmount)
+    {
+        healthAmount += healAmount;
+        if (healthAmount > 100) {  healthAmount = 100; }
+        healthBar.fillAmount = healthAmount / 100f;
     }
 }

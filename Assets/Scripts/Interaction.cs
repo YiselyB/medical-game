b8121f6c53cd0c4b827ebdc975d4e1d6bf79b576
Yisely <yisely@boominmedia.biz>
2024-08-18 17:11:17 -0700

    Fixed the transparency

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-//using static System.Net.Mime.MediaTypeNames;
 
 public class Interaction : MonoBehaviour, IInteractable
 {
@@ -17,9 +16,22 @@ public class Interaction : MonoBehaviour, IInteractable
 
     public void Interact()
     {
-        if(interactNum == 10)
+        switch (interactNum)
         {
-            Application.OpenURL("https://www.youtube.com/@BoominMediaDotBiz");
+            case 10://Youtbe URL
+                Application.OpenURL("https://www.youtube.com/@BoominMediaDotBiz");
+                break;
+            case 11://LinkedIn URL
+                Application.OpenURL("https://www.linkedin.com/company/boomin-media/");
+                break;
+            case 12://Facebook URL
+                Application.OpenURL("https://www.facebook.com/people/Boomin-Media/61557447059293/");
+                break;
+            case 13://Instagram URL
+                Application.OpenURL("https://www.instagram.com/boominmediadotbiz/");
+                break;
+            default:
+                break;
         }
         Debug.Log(interactNum);
         interactor = GameObject.Find("Scripts");
